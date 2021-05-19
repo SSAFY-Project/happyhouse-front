c555a91fe6472f7d23f65f1f7a6b81bd78558639
woongseob <55429912+woongseob12@users.noreply.github.com>
2021-05-20 02:04:59 +0900

    로그인 토큰처리
    1. map형태로 전달
    - 아이디, 비밀번호, 관심리스트
    2. 로그인 구현했음

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,58 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios"
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    userInfo: null,
+    isLogin: false,
+    isLoginError: false,
+  },
+  mutations: {
+    loginSuccess(state, payload) {
+      state.isLogin = true;
+      state.isLoginError = false;
+      state.userInfo = payload;
+    },
+    loginError(state) {
+      state.isLogin = false;
+      state.isLoginError = true;
+      state.userInfo = null;
+    }
+  },
+  actions: {
+    // 로그인
+    login({ commit }, loginObj) {
+      axios
+        .post("https://reqres.in/api/login", loginObj)
+        .then(res => {
+          // 성공했으므로 토큰 받음
+          let token = res.data.token;
+          let config = {
+            headers: {
+              "access-token": token,
+            }
+          }
+          axios
+            .get("https://reqres.in/api/login/2", config)
+            .then(response => {
+              let userInfo = {
+                id: response.data.data.id,
+                nickname: response.data.data.nickname,
+                favorite: response.data.data.favorite,
+              }
+              commit("loginSuccess", userInfo)
+            })
+            .catch(() => {
+              alert('아이디와 비밀번호를 확인하세요.')
+            })
+        })
+        .catch(() => {
+          alert('아이디와 비밀번호를 확인하세요.')
+        })
+    }
+  },
   modules: {},
 });
