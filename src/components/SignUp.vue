298be2eb24a4153faea2fc85572adf84f01b0694
woongseob <55429912+woongseob12@users.noreply.github.com>
2021-05-20 01:22:17 +0900

    회원가입 컴포넌트
    1. 아이디
    - 이메일 형식 아닐 경우 문구 출력
    - 아이디 중복(일단은 a@a.a로 설정)일 경우 중복 문구 출력 // 추후에 db랑 연결해서 봐야함
    - 아이디 공란일 경우 문구 출력
    2. 비밀번호 
    - 4글자 이하 문구 출력
    - 대소문자,특수문자하나씩 넣는거 함수 구현 예정
    3. 이름 
    4. 별명
    5. 관심정도
    6. 버튼

---
+++ src/components/SignUp.vue
@@ -0,0 +1,93 @@
+<template>
+    <v-form>
+        <v-container>
+            <v-card>
+                <v-card-title>회원 가입</v-card-title>
+                <v-card-text>
+                    <v-row class="mb-3">
+                        <v-col cols="6" md="8">
+                            <v-text-field
+                                v-model="userId"
+                                :rules="idRules"
+                                label="아이디"
+                                required="required"/>
+                        </v-col>
+                        <v-col cols="6" md="8">
+                            <v-text-field
+                                v-model="userPw"
+                                :rules="pwRules"
+                                label="비밀번호"
+                                required="required"/>
+                        </v-col>
+
+                        <v-col cols="6" md="8">
+                            <v-text-field
+                                v-model="userName"
+                                :rules="nameRules"
+                                :counter="10"
+                                label="이름"
+                                required="required"/>
+                        </v-col>
+
+                        <v-col cols="6" md="8">
+                            <v-text-field
+                                v-model="nickname"
+                                :rules="nicknameRules"
+                                :counter="10"
+                                label="별명"
+                                required="required"/>
+                        </v-col>
+                    </v-row>
+                    <v-card class="mb-3">
+                        <v-card-title>관심있는 것</v-card-title>
+                        <v-checkbox label="카페" value="카페"/>
+                        <v-checkbox label="정류장" value="정류장"/>
+                        <v-checkbox label="헬스장" value="헬스장"/>
+                    </v-card>
+                    <v-btn class="mr-4" type="submit">회원 가입</v-btn>
+                    <v-btn>초기화</v-btn>
+                </v-card-text>
+            </v-card>
+
+        </v-container>
+    </v-form>
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+                valid: false,
+                userId: '',
+                userPw: '',
+                userName: '',
+                nickname: '',
+                interest: [],
+
+                idRules: [
+                    v => !!v || '아이디를 입력하세요.',
+                    v => /.+@.+/.test(v) || '이메일 형식이 아닙니다.',
+                    v => v !== 'a@a.a' || '이미 등록된 아이디입니다.',
+                    // 이미 등록된 아이디처리는 DB를 통해 찾아봐야 함.
+                ],
+
+                pwRules: [
+                    v => !!v || '비밀번호를 입력하세요.',
+                    v => v.length >= 4 || '비밀번호는 4글자 이상이여야 합니다.'
+                ],
+
+                nameRules: [
+                    v => !!v || '이름을 입력하세요.',
+                    v => v.length <= 10 || '이름이 10글자 넘는건 선넘었지..'
+                ],
+
+                nicknameRules: [
+                    v => !!v || '별명을 입력하세요.',
+                    v => v.length >= 2 || '별명은 2글자 이상이여야 합니다.',
+                    v => v.length <= 10 || '별명은 10글자 이하여야 합니다.'
+                ]
+            }
+        }
+    }
+</script>
