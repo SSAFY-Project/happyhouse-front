3771956b2cc6a16ff8e1121efd7bd3780ffc33a3
woongseob <55429912+woongseob12@users.noreply.github.com>
2021-05-20 02:22:59 +0900

    토큰으로 로그인
    1. 새로고침시에도 유지되도록 설정

--- src/store/index.js
+++ src/store/index.js
@@ -24,35 +24,45 @@ export default new Vuex.Store({
   },
   actions: {
     // 로그인
-    login({ commit }, loginObj) {
+    login({ dispatch }, loginObj) {
       axios
         .post("https://reqres.in/api/login", loginObj)
         .then(res => {
           // 성공했으므로 토큰 받음
-          let token = res.data.token;
-          let config = {
-            headers: {
-              "access-token": token,
-            }
-          }
-          axios
-            .get("https://reqres.in/api/login/2", config)
-            .then(response => {
-              let userInfo = {
-                id: response.data.data.id,
-                nickname: response.data.data.nickname,
-                favorite: response.data.data.favorite,
-              }
-              commit("loginSuccess", userInfo)
-            })
-            .catch(() => {
-              alert('아이디와 비밀번호를 확인하세요.')
-            })
+          let token = res.data.token
+          // 토큰 로컬스토리지에 저장해야함(새로고침 팅김 방지)
+          localStorage.setItem("access_token", token)
+          dispatch("getUserInfo")
         })
         .catch(() => {
           alert('아이디와 비밀번호를 확인하세요.')
         })
-    }
+    },
+    getUserInfo({ commit }) {
+      // 로컬에 있는 토큰 받기
+      let token = localStorage.getItem("access_token")
+      let config = {
+        headers: {
+          "access-token": token,
+        }
+      }
+      axios
+        .get("https://reqres.in/api/login/2", config)
+        .then(response => {
+          let userInfo = {
+            id: response.data.data.id,
+            nickname: response.data.data.nickname,
+            favorite: response.data.data.favorite,
+          }
+          commit("loginSuccess", userInfo)
+        })
+        .catch(() => {
+          alert('아이디와 비밀번호를 확인하세요.')
+        })
+    },
+    /*logout({ commit }) {
+      commit("logout")
+    }*/
   },
   modules: {},
 });
