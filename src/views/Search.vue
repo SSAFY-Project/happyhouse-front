73edb47c04722a5233cff951706303bd730b9ac1
woongseob <55429912+woongseob12@users.noreply.github.com>
2021-05-24 13:27:40 +0900

    해시태그 기능 추가

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,18 +1,103 @@
 <template>
-<div>
-  <h1>필터기능있는 검색</h1>
-  <h1>필터기능있는 검색</h1>
-  <h1>필터기능있는 검색</h1>
-  <h1>필터기능있는 검색</h1>
-  </div>
+  <v-main>
+    <v-container>
+      <v-expansion-panels>
+        <v-expansion-panel>
+          <v-expansion-panel-header> HashTag </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-container>
+              <v-row>
+                <v-col cols="2">
+                  <v-checkbox
+                    label="대출 지원"
+                    value="대출 지원"
+                    v-model="interests"
+                  />
+                </v-col>
+                <v-col cols="2">
+                  <v-checkbox
+                    label="주차 가능"
+                    value="주차 가능"
+                    v-model="interests"
+                  />
+                </v-col>
+                <v-col cols="2">
+                  <v-checkbox
+                    label="역세권"
+                    value="역세권"
+                    v-model="interests"
+                  />
+                </v-col>
+                <v-col cols="2">
+                  <v-checkbox
+                    label="공기 좋음"
+                    value="공기 좋음"
+                    v-model="interests"
+                  />
+                </v-col>
+                <v-col cols="2">
+                  <v-checkbox
+                    label="펜트하우스"
+                    value="펜트하우스"
+                    v-model="interests"
+                  />
+                </v-col>
+                <v-col cols="2">
+                  <v-checkbox
+                    label="옵션 1"
+                    value="옵션 1"
+                    v-model="interests"
+                  />
+                </v-col>
+                <v-col cols="2">
+                  <v-checkbox
+                    label="옵션 2"
+                    value="옵션 2"
+                    v-model="interests"
+                  />
+                </v-col>
+                <v-col cols="2">
+                  <v-checkbox
+                    label="옵션 3"
+                    value="옵션 3"
+                    v-model="interests"
+                  />
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+
+      <v-chip
+        class="ma-2"
+        close
+        color="green"
+        text-color="white"
+        v-for="(item, idx) in interests"
+        :key="idx"
+        @click:close="interests.splice(idx, 1)"
+      >
+        # {{ item }}
+      </v-chip>
+    </v-container>
+  </v-main>
 </template>
 
 <script>
-export default {
+//import Slider from "@vueform/slider";
 
-}
+export default {
+  /*components: {
+    Slider,
+  },*/
+  data() {
+    return {
+      interests: [],
+      price: [0, 99999999999],
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style scoped></style>
