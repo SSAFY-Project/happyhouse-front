908081cef97115ee76456722ef3605dea06d2737
woongseob <55429912+woongseob12@users.noreply.github.com>
2021-05-26 13:01:38 +0900

    drawer 추가

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,84 @@
 <template>
-  <v-app>
-    <NavigationBar />
+  <v-app class="app" id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      <v-container class="userImg">
+        <img class="profileImg" src="./assets/profile.jpg" />
+      </v-container>
+      <v-list-item align-center>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            김찬호 님 / admin
+          </v-list-item-title>
+          <v-list-item-subtitle> kimhae@chengnyeon.com </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list dense nav>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-cog</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>회원 관리</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-heart</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>관심 지역</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-account</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>회원 정보</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <NavigationBar @showDrawer="showDrawer()" />
     <router-view />
   </v-app>
 </template>
 
 <script>
-import NavigationBar from "./components/NavigationBar";
+import NavigationBar from "./components/NavigationBar.vue";
+
 export default {
   name: "App",
   components: {
     NavigationBar,
   },
-}
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+  methods: {
+    showDrawer() {
+      this.drawer = !this.drawer;
+    },
+  },
+};
 </script>
 
-<style></style>
+<style>
+::-webkit-scrollbar {
+  display: none;
+}
+
+.userImg {
+  width: 50%;
+  padding-top: 100%;
+  overflow: hidden;
+}
+.profileImg {
+  width: 100%;
+  height: 100%;
+  border-radius: 100%;
+  object-fit: cover;
+}
+</style>
