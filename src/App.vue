6260f0cc72b4cbfd0731462b1cb233657615ce12
김성민 <mbp@Macbook-Air.local>
2021-05-28 00:55:16 +0900

    router

--- src/App.vue
+++ src/App.vue
@@ -10,14 +10,22 @@
             게스트 / Guest
           </v-list-item-title>
           <v-list-item-title class="title" v-if="this.$store.state.userInfo !== null">
-            {{this.$store.state.userInfo.userName}} / {{this.$store.state.userInfo.userRole}}
+            {{ this.$store.state.userInfo.userName }} / {{ this.$store.state.userInfo.userRole }}
           </v-list-item-title>
-          <v-list-item-subtitle v-if="this.$store.state.userInfo !== null"> {{this.$store.state.userInfo.userEmail}} </v-list-item-subtitle>
-
+          <v-list-item-subtitle v-if="this.$store.state.userInfo !== null">
+            {{ this.$store.state.userInfo.userEmail }}
+          </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
       <v-list dense nav>
-        <v-list-item router-link to="/manager" v-if="this.$store.state.userInfo !== null && this.$store.state.userInfo.userRole ==='ROLE_ADMIN'">
+        <v-list-item
+          router-link
+          to="/manager"
+          v-if="
+            this.$store.state.userInfo !== null &&
+            this.$store.state.userInfo.userRole === 'ROLE_ADMIN'
+          "
+        >
           <v-list-item-icon>
             <v-icon>mdi-cog</v-icon>
           </v-list-item-icon>
@@ -25,22 +33,22 @@
             <v-list-item-title>회원 관리</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-          <v-list-item @click="checkUserFavorite" >
-            <v-list-item-icon>
-              <v-icon>mdi-heart</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>관심 지역</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item @click="checkUserInfo">
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>회원 정보</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>      
+        <v-list-item @click="checkUserFavorite">
+          <v-list-item-icon>
+            <v-icon>mdi-heart</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>관심 지역</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="checkUserInfo">
+          <v-list-item-icon>
+            <v-icon>mdi-account</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>회원 정보</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
       </v-list>
     </v-navigation-drawer>
     <NavigationBar @showDrawer="showDrawer()" />
@@ -60,10 +68,10 @@ export default {
     return {
       drawer: false,
       userInfo: {
-        userName: '게스트',
-        userRole: 'guest',
-        userEmail: '',
-      }
+        userName: "게스트",
+        userRole: "guest",
+        userEmail: "",
+      },
     };
   },
   methods: {
@@ -71,23 +79,21 @@ export default {
       this.drawer = !this.drawer;
     },
     checkUserFavorite() {
-      if(this.$store.state.userInfo === null){
-        alert('로그인 후 이용하세요.');
+      if (this.$store.state.userInfo === null) {
+        alert("로그인 후 이용하세요.");
         window.location.href = "/signin";
-      }
-      else{
-        this.$router.push('/search');
+      } else {
+        this.$router.push("/favorite");
       }
     },
     checkUserInfo() {
-      if(this.$store.state.userInfo === null){
-        alert('로그인 후 이용하세요.');
+      if (this.$store.state.userInfo === null) {
+        alert("로그인 후 이용하세요.");
         window.location.href = "/signin";
+      } else {
+        this.$router.push("/useredit");
       }
-      else{
-        this.$router.push('/useredit');
-      }
-    }
+    },
   },
 };
 </script>
